cbce841abb60bd2830f87d44958451704463328a
kronemyermakena <makenakronemyer@gmail.com>
2017-12-01 10:46:47 -0800

    part or assignment 1

--- ECSJavaProject/src/FirstProg.java
+++ ECSJavaProject/src/FirstProg.java
@@ -4,7 +4,7 @@ public class FirstProg
 	
 	public static void main(String[] args)
 	{
-		System.out.println("CCA!");
 		
+		System.out.println("CCA");
 	}
 }
---
+++ ECSJavaProject/src/YourInitials.java
@@ -0,0 +1,23 @@
+
+public class YourInitials
+{
+	public static void main(String[] args)
+	{
+		System.out.println("M     M  M     M  K   K");	
+		System.out.println("MM   MM  MM   MM  K  K");
+		System.out.println("MM   MM  MM   MM  K K");
+		System.out.println("M  M  M  M  M  M  KK"); 
+		System.out.println("M     M  M     M  K K");
+		System.out.println("M     M  M     M  K  K");
+		System.out.println("M     M  M     M  K   K");
+	
+		
+
+
+
+	}
+}
+
+
+
+
