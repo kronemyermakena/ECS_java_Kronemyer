f548aa4eba8b63639be8789c62bd7fa524e43211
kronemyermakena <makenakronemyer@gmail.com>
2017-12-05 10:04:48 -0800

    Part of Assignment 2

---
+++ ECSJavaProject/src/QuestionsforAssignments1and2.java
@@ -0,0 +1,8 @@
+
+public class QuestionsforAssignments1and2 
+{
+// 1. If you just put 4 instead of 4.0 the answers don't change. The difference between = and == is that == is an equal to operator, 
+	// 2. while = just assigns an expression to a variable.
+		//3. You're declaring what variables you'll be using
+	    	//4. 
+}
---
+++ ECSJavaProject/src/StillUsingVariables.java
@@ -0,0 +1,14 @@
+
+public class StillUsingVariables
+{
+	public static void main(String[] args)
+	{
+		
+		String myName;
+		int mygradYear;
+			myName = "Makena";
+			mygradYear = 2019;
+				System.out.println(" My name is " + myName + " and I'll graduate in " + mygradYear  );
+				
+	}
+}
