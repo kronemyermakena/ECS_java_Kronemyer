d52c4fe496b0ac9feedcb0100a8aecd60f7f4db5
kronemyermakena <makenakronemyer@gmail.com>
2017-12-06 10:57:06 -0800

    Assignment 3

---
+++ Keyboard Inputs/src/Agein5.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+	public class Agein5
+{
+		public static void main(String[] args)
+	{
+			Scanner keyboard = new Scanner(System.in);
+			
+				String name; 
+				int age;
+					
+					System.out.print(" Hello. What is your name ? ");
+					name = keyboard.next();
+				
+					System.out.println(" Hi, " + name + ". How old are you ? ");
+					age = keyboard.nextInt();
+					
+					System.out.println(" Did you know that in five years you will be " + (age + 5) + " years old? And five years ago you were " + (age - 5) + "! Imagine that! " );
+					
+					keyboard.close();
+			
+	}
+
+}
+	
+ 
---
+++ Keyboard Inputs/src/Assignment3Questions.java
@@ -0,0 +1,7 @@
+
+public class Assignment3Questions 
+{
+	//1. The first line of code imports 
+		//2. 
+			//3. 
+}
